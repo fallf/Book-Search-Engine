24bd15e4771946c59becf1b2e698ddd1db0f836e
fallf <87919117+fallf@users.noreply.github.com>
2022-01-20 14:17:13 -0500

    update

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const { authMiddleware } = require('./utils/auth');
 
 // Set up apollo server
 const app = express();
-const PORT = process.env.PORT || 3030;
+const PORT = process.env.PORT || 3000;
 async function startServer() {
   apolloServer = new ApolloServer({
       typeDefs,
@@ -30,12 +30,15 @@ app.use(express.json());
 // if we're in production, serve client/build as static assets
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  });
 }
 
 // app.use(routes);
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
-});
+// app.get('*', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../client/build/index.html'));
+// });
 
 db.once('open', () => {
   app.listen(PORT, () => {
