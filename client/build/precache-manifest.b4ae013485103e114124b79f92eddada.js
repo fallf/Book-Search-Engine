44953ca7431a5971c40204388885f92522ccd505
fallf <87919117+fallf@users.noreply.github.com>
2022-01-20 13:30:54 -0500

    updating node packages

---
+++ client/build/precache-manifest.b4ae013485103e114124b79f92eddada.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a745cb7140e8283032f39597b3a47d9",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cfcae75de9d6821bcba2",
+    "url": "/static/css/2.3661fc73.chunk.css"
+  },
+  {
+    "revision": "97fc542b1452a475e4a5",
+    "url": "/static/css/main.a13d3d6a.chunk.css"
+  },
+  {
+    "revision": "cfcae75de9d6821bcba2",
+    "url": "/static/js/2.901473f0.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/static/js/2.901473f0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "97fc542b1452a475e4a5",
+    "url": "/static/js/main.c9ecc8cb.chunk.js"
+  },
+  {
+    "revision": "9b4e396c183e42c1fa5c",
+    "url": "/static/js/runtime-main.09b85ec0.js"
+  }
+]);
